1a873302fc67b15e942a4e304c6ddfe1f3025586
Benjamin-Barda <benjamin.barda@hcl.com>
2023-07-28 09:09:17 +0200

    Hello

--- lua/benjo/packer.lua
+++ lua/benjo/packer.lua
@@ -1,5 +1,19 @@
 vim.cmd [[packadd packer.nvim]]
 
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
@@ -43,7 +57,25 @@ return require('packer').startup(function(use)
     }
     use('theprimeagen/harpoon')
 
+    use {'echasnovski/mini.nvim', branch = 'stable'}
+
     use('ggandor/leap.nvim')
-    use('github/copilot.vim')
+
+    use {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        event = "InsertEnter",
+        config = function()
+            require("copilot").setup({
+                filetypes = {
+                    yaml = true
+                }
+            })
+        end,
+    }
+
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 
 end)
