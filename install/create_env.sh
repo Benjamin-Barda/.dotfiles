8c8a0d32a53e9499d989631efc05823fb3ce6217
Benjamin <80880329+Benjamin-Barda@users.noreply.github.com>
2024-02-22 16:56:16 +0100

    Create create_env.sh

---
+++ install/create_env.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# zsh
+sudo apt install zsh
+chsh -s $(which zsh)
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+# Lazy git
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+# i3 + tmux
+sudo apt install i3 tmux
